56a8889677542ab06191ab6b9ee7b2fec9f1947f
mdobro <mdobro@umich.edu>
2017-06-24 02:48:09 -0400

    nice feeling ball physics

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -6,7 +6,7 @@ public class FollowCam : MonoBehaviour {
 
     public GameObject player;
 
-    private Vector3 offset;
+    public Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
